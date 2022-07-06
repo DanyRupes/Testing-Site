04c6c5b4af22058d09ef5a91f6c6f088fe6bd0f2
dany <danyrupes26@gmail.com>
2022-07-06 19:52:23 +0530

    lllg

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing the generated config
-var firebaseConfig =  {
+var firebaseConfig = {
     apiKey: "AIzaSyCud89dpES9zKc_3Zcz9EDG8dwsI-XblTE",
     authDomain: "develop-jz.firebaseapp.com",
     projectId: "develop-jz",
@@ -11,21 +11,24 @@ var firebaseConfig =  {
     messagingSenderId: "722040087904",
     appId: "1:722040087904:web:a791336cf7cd40e53aaa05",
     measurementId: "G-4L1KL33EX5"
-  };
+};
 
 firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
+messaging.onBackgroundMessage(function (payload) {
+    console.log('Received background message ', payload);
 
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
+    console.log({
+        notificationTitle,
+        notificationOptions
+    })
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
 });

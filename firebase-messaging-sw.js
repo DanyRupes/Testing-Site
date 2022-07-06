512913c60775da781819cb36cfaeb491c6b6c199
dany <danyrupes26@gmail.com>
2022-07-06 20:10:00 +0530

    logs

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,6 +17,7 @@ firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
+var clickUrl;
 
 messaging.onBackgroundMessage(function (payload) {
     console.log('Received background message ', payload);
@@ -24,9 +25,19 @@ messaging.onBackgroundMessage(function (payload) {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        payload
+        icon: "https://picsum.photos/id/237/200/300",
+        actions: [
+            {
+                action: 'view-ticket',
+                title: 'View Ticket',
+                icon: '/ui/img/icons/ticket-icon.png',
+                click_url: "https://gozen.io"
+            }
+        ],
     };
 
+    clickUrl = payload.data.click_url;
+    console.log("clickUrl 1", clickUrl, )
     self.registration.showNotification(notificationTitle,
         notificationOptions);
 });
@@ -35,27 +46,43 @@ messaging.onBackgroundMessage(function (payload) {
 self.addEventListener('notificationclick', (event) => {
 
     event.notification.close();
-    
+
     console.log("event", event)
-    console.log("event.notification.data", event.notification.data)
-    
-    const url = event.notification.data.url
-    if (!url) return
-  
-    event.waitUntil(
-      self.clients
+    if (!event.action) {
+        return;
+    }
+
+    event.waitUntil(self.clients
         .matchAll({ type: 'window', includeUncontrolled: true })
         .then((clientsArr) => {
-          const hadWindowToFocus = clientsArr.some((windowClient) =>
-            windowClient.url === url ? (windowClient.focus(), true) : false
-          )
-  console.log({hadWindowToFocus})
-          if (!hadWindowToFocus)
+            console.log("click")
             self.clients
-              .openWindow(url)
-              .then((windowClient) =>
-                windowClient ? windowClient.focus() : null
-              )
+                .openWindow(url)
+                .then((windowClient) =>
+                    windowClient ? windowClient.focus() : null
+                )
         })
     )
-  })
+})
+
+
+
+
+
+
+
+// event.waitUntil(self.clients
+//     .matchAll({ type: 'window', includeUncontrolled: true })
+//     .then((clientsArr) => {
+//         const hadWindowToFocus = clientsArr.some((windowClient) =>
+//             windowClient.url === url ? (windowClient.focus(), true) : false
+//         )
+//         console.log({ hadWindowToFocus })
+//         if (!hadWindowToFocus)
+//             self.clients
+//                 .openWindow(url)
+//                 .then((windowClient) =>
+//                     windowClient ? windowClient.focus() : null
+//                 )
+//     })
+// )
